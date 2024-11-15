b1e795993f95585eef2e5d1d2c252c5a15a46070
gitJEO <jeretobermeyer@gmail.com>
2024-11-15 03:19:36 -0600

    Update bps_m03.py

--- bps_m03.py
+++ bps_m03.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 # ----------------------------------------------------------------------#
 # FILE PROPERTIES AND AUTHOR INFO                                       #
 #   fileName            basic_port_scanner                              #
-#   fileType            Plain Text Document (.txt)                      #
-#   language            Python 3 (11.9)                                 #
+#   fileType            Python 3 Script (.py)                           #
+#   language            Python (v3.12)                                  #
 #   author              Jeret E. Obermeyer                              #
 #   courseNum           IS-4543-002                                     #
 #                                                                       #
@@ -11,38 +12,43 @@
 #   projDesc            Port-scanning tool that analyzes a target ma-   #
 #                       -chine for open TCP ports and attempts to id-   #
 #                       -entify services running associated with them   #
-#   msNum               Milestone 03                                    #
-#   msWorkStart         10/25/2024                                      #
-#   msWorkEnd           11/07/2024                                      #
+#   msNum               Milestone 04                                    #
+#   msWorkStart         11/07/2024                                      #
+#   msWorkEnd           11/15/2024                                      #
 #                                                                       #
 #                 Copyright (c) 2024, UT at San Antonio                 #
 # ----------------------------------------------------------------------#
 
 # Python Standard Library
-# MODULES                           LIBRARY TYPE                                                                      SOURCE CODE
-import socket                       # LIBRARY 01:  low-level networking interface                                      https://github.com/python/cpython/tree/3.13/Lib/socket.py
-import argparse                     # LIBRARY 02:  Parser for command-line options, arguments and subcommands          https://docs.python.org/3/library/argparse.html
-import time                         # LIBRARY 04:  Time access and conversions                                         https://docs.python.org/3/library/time.html#module-time
-import csv                          # LIBRARY 05:  File reading and writing                                            https://github.com/python/cpython/blob/3.13/Lib/csv.py
-import asyncio                      # LIBRARY 06:  Concurrent programming design for high-performance network queues   https://realpython.com/async-io-python/
-import colorama                     # LIBRARY 07:  Import colorama                                                     https://github.com/tartley/colorama
-import re                           # LIBRARY 08:  For regular expressions to clean banner                             https://github.com/python/cpython/tree/3.13/Lib/re/
-import ipaddress                    # LIBRARY 09:  IPv4/IPv6 manipulation library                                      https://github.com/python/cpython/blob/3.13/Lib/ipaddress.py 
-from tabulate import tabulate       # LIBRARY 10:  Displays results in a table with columns for specified fields       https://github.com/astanin/python-tabulate 
-from colorama import Fore, Style    # LIBRARY 11:  Use colors to highlight open ports or errors                        https://github.com/tartley/colorama
-from itertools import islice        # LIBRARY 12:  Functions creating iterators for efficient looping                  https://docs.python.org/3/library/itertools.html
+# MODULES                           LIBRARY TYPE                                                                        SOURCE CODE
+import socket                       # LIBRARY 01:  low-level networking interface                                       https://github.com/python/cpython/tree/3.13/Lib/socket.py
+import argparse                     # LIBRARY 02:  Parser for command-line options, arguments and subcommands           https://docs.python.org/3/library/argparse.html
+import time                         # LIBRARY 04:  Time access and conversions                                          https://docs.python.org/3/library/time.html#module-time
+import csv                          # LIBRARY 05:  File reading and writing                                             https://github.com/python/cpython/blob/3.13/Lib/csv.py
+import asyncio                      # LIBRARY 06:  Concurrent programming design for high-performance network queues    https://realpython.com/async-io-python/
+import colorama                     # LIBRARY 07:  Import colorama                                                      https://github.com/tartley/colorama
+import re                           # LIBRARY 08:  For regular expressions to clean banner                              https://github.com/python/cpython/tree/3.13/Lib/re/
+import ipaddress                    # LIBRARY 09:  IPv4/IPv6 manipulation library                                       https://github.com/python/cpython/blob/3.13/Lib/ipaddress.py 
+from tabulate import tabulate       # LIBRARY 10:  Displays results in a table with columns for specified fields        https://github.com/astanin/python-tabulate 
+from colorama import Fore, Style    # LIBRARY 11:  Use colors to highlight open ports or errors                         https://github.com/tartley/colorama
+from itertools import islice        # LIBRARY 12:  Functions creating iterators for efficient looping                   https://docs.python.org/3/library/itertools.html
+import traceback                    # LIBRARY 13:  Print or retrieve a stack traceback                                  https://docs.python.org/3/library/traceback.html
 
-colorama.init()                     # Initialized colorama for tabular output formatting
-
+# PLUGINS                                                                                                               PATH
+from service_plugins import service_plugins, register_plugin                                                        #   /home/kali/Desktop/Programs/service_plugins.py
 
 # CONSTANTS
 parser = argparse.ArgumentParser(description="Asynchronous Multi-Target Port Scanner")                                                              # The description of the custom argument/command line
-parser.add_argument("target", help="Target IP address, hostname, or CIDR range to scan")                                                            # param1   Sets the target IP address to scan and explains functionality when help() is called
-parser.add_argument("--start_port", type = int, default = 1, help = "Start of port range to scan")                                                  # param2   Sets the starting port range to scan based on IP address in param1 
-parser.add_argument("--end_port", type = int, default = 1024, help = "End of port range to scan")                                                   # param2   Sets the ending port range to scan based on IP address in param1 
-parser.add_argument("--csv_path", default = "/home/kali/Desktop/service-names-port-numbers.csv", help = "Path to the service names CSV file")       # param3   Sets the preferred directory for scanning criteria
-parser.add_argument("--timeout", type=float, default=0.5, help="Socket timeout for each port")                                                      # param4   Sets the timeout value for each port scan
+parser.add_argument("target", help="Target IP address, hostname, or CIDR range to scan")                                                            # param1    Sets the target IP address to scan and explains functionality when help() is called
+parser.add_argument("--start_port", type = int, default = 1, help = "Start of port range to scan")                                                  # param2    Sets the starting port range to scan based on IP address in param1 
+parser.add_argument("--end_port", type = int, default = 1024, help = "End of port range to scan")                                                   # param2    Sets the ending port range to scan based on IP address in param1 
+parser.add_argument("--csv_path", default = "/home/kali/Desktop/service-names-port-numbers.csv", help = "Path to the service names CSV file")       # param3    Sets the preferred directory for scanning criteria
+parser.add_argument("--timeout", type=float, default=0.5, help="Socket timeout for each port")                                                      # param4    Sets the timeout value for each port scan
+parser.add_argument("--batch_size", type=int, default=100, help="Number of concurrent connections")                                                 # param5    Sets the batch size value for the scan
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output for debugging")                                                   # param6    Sets the scan output to contain line-by-line information on each scan
+
 args = parser.parse_args()                                                                                                                          # Runs the parser and places the extracted data in a argparse.Namespace object (see: ArgumentParser.parse_args())
+colorama.init()                                                                                                                                     # Initialized colorama for tabular output formatting
 
 #   VAR 01:         load_port_service_mapping
 #   DESCRIPTION:    Parses and filters data from specified .csv file
@@ -102,75 +108,65 @@ def clean_banner(banner):
 
 #   VAR 04:         scan_port
 #   DESCRIPTION:    Establishing socket-to-port connections
+# Defines the adjustable maximum number of concurrent tasks
+semaphore = asyncio.Semaphore(1000)
+
 async def scan_port(target, port, timeout):
-    try:
-        # Attempt to open an asynchronous TCP connection to the target port
-        reader, writer = await asyncio.wait_for(
-            # Records timeout as constant in relation to target and port number while async scan occurs
-            asyncio.open_connection(target, port), timeout=timeout
-        )
-        # Special handling for RDP (port 3389): mark as open without requesting further data
-        if port == 3389:
-            # Immediately closes the connection without attempting to read scanned banner
+    async with semaphore:
+        try:
+            # Attempts to open an asynchronous TCP connection to the target port
+            reader, writer = await asyncio.wait_for(asyncio.open_connection(target, port), timeout=timeout)
+            # Refers to service_plugins.py and stores as a constant
+            plugin_func = service_plugins.get(port)
+            # Checks for unique services from plugin
+            if plugin_func:
+                # Stores banners from unique services if open
+                banner = await plugin_func(reader, writer)
+            # Task for non-unique ports/banners
+            else:
+                try:
+                    # Sends a generic newline to the port to potentially prompt a response
+                    writer.write(b"\r\n")
+                    # Ensures data is sent to the target port
+                    await writer.drain()
+                    # Reads up to 4096 bytes from target port before timeout
+                    data = await asyncio.wait_for(reader.read(4096), timeout=1)
+                    # Parses transmitted data into string value
+                    banner = data.decode('utf-8', errors='ignore').strip()
+                    if banner:
+                        banner = clean_banner(banner)
+                    else:
+                        banner = 'No banner'
+                except Exception:
+                    banner = 'No banner'
+
+            # Closes the connection once reading is complete
             writer.close()
             await writer.wait_closed()
-            # Retrieves the service name from port mapping
+
+            # Cleans and formats the banner whenever able
             service_name = get_service_name(port)
+
+            # Checks if banner is longer than 80 characters
+            if len(banner) > 80:
+                banner = banner[:80] + '...'
             return {
                 'Port': port,
                 'Service': service_name,
                 'Status': 'Open',
-                'Banner': 'No banner (RDP special case)'
+                'Banner': banner
             }
-        # TODO:     Port range greater than between 1 to 5000 will continually error out
-        #           unless I keep this weirdly redundant special case for Port 3389; will
-        #           probably stay unless I can figure out a workaround that doesn't result
-        #           in spending a concerning amount of time on this one problem.
-
-        # For other ports, attempt to retrieve the banner
-        banner = ''
-        try:
-            # Sends a generic newline to the port to potentially prompt a response
-            writer.write(b"\r\n")
-            # Ensures data is sent to the target port
-            await writer.drain()
-            # Reads up to 4096 bytes from target port before timeout
-            data = await asyncio.wait_for(reader.read(4096), timeout=timeout)
-            # Parses transmitted data into string value
-            banner = data.decode('utf-8', errors='ignore').strip()
-        except Exception:
-            pass
-        
-        # Closes the connection once reading is complete
-        writer.close()
-        await writer.wait_closed()
-        
-        # Cleans and formats the banner whenever able
-        service_name = get_service_name(port)
-        
-        # Checks for unnecessary tags or metadata and removes them
-        if banner:
-            banner = clean_banner(banner)
-        # Checks if banner is longer than 80 characters
-        if len(banner) > 80:
-            banner = banner[:80] + '...'
-        
-        return {
-            'Port': port,
-            'Service': service_name,
-            'Status': 'Open',
-            'Banner': banner or 'No banner'
-        }
-    except (asyncio.TimeoutError, ConnectionRefusedError):
-        # Ignore common errors like timeouts and connection refusals
-        return None
-    except Exception as e:
-        # Capture unexpected errors with port information
-        return {'Port': port, 'Error': f"Unexpected error: {e}"}
+        except Exception as e:
+            # Stores common errors like timeouts and connection refusals as verbose data
+            if args.verbose:
+                print(f"Exception when connecting to port {port}: {type(e).__name__}: {e}")
+                # Stack traceback for exceptions
+                traceback.print_exc()
+            return None
 
 #   VAR 05:         port_scan
 #   DESCRIPTION:    Port range loopback for port connectivity
-async def port_scan(target, start_port, end_port, timeout, batch_size=200):
+async def port_scan(target, start_port, end_port, timeout, batch_size=1000):
     # Begins runtime
     start_time = time.time()
     print(f"Scanning {target} from port {start_port} to {end_port}")
@@ -238,10 +234,11 @@ async def scan_network(target_range, start_port, end_port, timeout):
     try:
         # Parses target range and iterates over each host IP
         network = ipaddress.ip_network(target_range, strict=False)
+        tasks = []
         # Checks for host IP in Local Area Network
         for ip in network.hosts():
-            print(f"\nStarting scan on {ip}")
-            await port_scan(str(ip), start_port, end_port, timeout)
+            tasks.append(port_scan(str(ip), start_port, end_port, timeout))
+            await asyncio.gather(*tasks)
     except ValueError as e:
         # Handle invalid network input
         print(f"Invalid network: {e}")
